7d6f1006cd13841587cde3ff10539232e20d052f
Mia Bueno <mia.bueno101@gmail.com>
2021-01-28 11:46:55 +1100

    add event listeners to all drum buttons

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("working!");
+allButtons = document.querySelectorAll("button.drum")
+
+for (var i = 0; i < allButtons.length; i++) {
+	allButtons[i].addEventListener("click", handleClick)
+}
+
+function handleClick() {
+	alert("I got clicked!");
+}
