15601e7f73ea4af859c45b6d5a1190b63e110733
Mia Bueno <mia.bueno101@gmail.com>
2021-01-28 14:13:59 +1100

    give buttons picture backgrounds and corresponding event listening sounds

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
-allButtons = document.querySelectorAll("button.drum")
+
+// Add even listeners to all the buttons
+allButtons = document.querySelectorAll("button.drum");
 
 for (var i = 0; i < allButtons.length; i++) {
-	allButtons[i].addEventListener("click", handleClick)
+	allButtons[i].addEventListener("click", function(){
+		playSound(this.innerHTML);
+	});
 }
 
-function handleClick() {
-	alert("I got clicked!");
+// Add event listeners to keyboard keys
+document.addEventListener("keydown", function(){
+	playSound(event.key);
+});
+
+// puts together recipe of finding the corresponding sound file
+// and playing that file
+function playSound(keyLetter) {
+	soundFile = findSound(keyLetter);
+	makeSound(soundFile);
+}
+
+// returns sound file corresponding to key
+function findSound(keyLetter) {
+	switch (keyLetter) {
+		case "w":
+			return "sounds/tom-1.mp3"
+
+		case "a":
+			return "sounds/tom-2.mp3"
+
+		case "s":
+			return "sounds/tom-3.mp3"
+
+		case "d":
+			return "sounds/tom-4.mp3"
+
+		case "j":
+			return "sounds/crash.mp3"
+
+		case "k":
+			return "sounds/kick-bass.mp3"
+
+		case "l":
+			return "sounds/snare.mp3"
+
+		default: console.log(keyLetter);
+
+	}
+
+	return 
+}
+
+// plays sound as per sound file argument
+function makeSound(soundFile) {
+	var audio = new Audio(soundFile);
+	audio.play();
 }
