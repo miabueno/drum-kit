176a5d23ea13989ff323b8f4cdae25dd451666a7
Mia Bueno <mia.bueno101@gmail.com>
2021-01-28 16:17:35 +1100

    tidy up code

--- index.js
+++ index.js
@@ -25,31 +25,31 @@ function playSound(keyLetter) {
 function findSound(keyLetter) {
 	switch (keyLetter) {
 		case "w":
-			return "sounds/tom-1.mp3"
+			return "sounds/tom-1.mp3";
 
 		case "a":
-			return "sounds/tom-2.mp3"
+			return "sounds/tom-2.mp3";
 
 		case "s":
-			return "sounds/tom-3.mp3"
+			return "sounds/tom-3.mp3";
 
 		case "d":
-			return "sounds/tom-4.mp3"
+			return "sounds/tom-4.mp3";
 
 		case "j":
-			return "sounds/crash.mp3"
+			return "sounds/crash.mp3";
 
 		case "k":
-			return "sounds/kick-bass.mp3"
+			return "sounds/kick-bass.mp3";
 
 		case "l":
-			return "sounds/snare.mp3"
+			return "sounds/snare.mp3";
 
 		default: console.log(keyLetter);
 
 	}
 
-	return 
+	return; 
 }
 
 // plays sound as per sound file argument
@@ -58,12 +58,12 @@ function makeSound(soundFile) {
 	audio.play();
 }
 
-// given key, add pressed class styling to button
+// given key, add "pressed" class styling to button
 function buttonAnimation(keyLetter) {
 	var activeButton = document.querySelector("." + keyLetter);
 	activeButton.classList.add("pressed");
 
-	// wait 100 milliseconds
+	// wait 100 milliseconds to remove class "pressed" from button
 	setTimeout(function() {
     	activeButton.classList.remove("pressed");
   	}, 100);
